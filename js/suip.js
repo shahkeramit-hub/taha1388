348a68a9453f190630eb3fac06be5ac78d37b9bc
taha <shahkeramit@gmail.com>
2026-02-01 18:40:56 +0330

    start

---
+++ js/suip.js
@@ -0,0 +1,139 @@
+ var swiper = new Swiper(".webSwiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      effect: "fade",
+      autoplay: {
+        delay: 3400,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      loop: true,
+    });
+// سوییپر 2
+       var swiper = new Swiper(".mobaelSwiper", {
+      slidesPerView: 5,
+      centeredSlides: true,
+      spaceBetween: 30,
+      grabCursor: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      loop: true,
+    });
+    //سوییپر6
+          var swiper = new Swiper(".vachSwiper", {
+      slidesPerView: 5,
+      centeredSlides: true,
+      spaceBetween: 30,
+      grabCursor: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      loop: true,
+    });
+    //سوییپر3
+  var swiper = new Swiper(".terySwiper", {
+      slidesPerView: 5,
+      spaceBetween: 30,
+      loop: true,
+      freeMode: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+        //سوییپر4
+  var swiper = new Swiper(".talaSwiper", {
+      slidesPerView: 5,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+//سکشن5
+var swiper = new Swiper(".srojhSwiper", {
+      slidesPerView: 5,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+//سکشن6
+var swiper = new Swiper(".berozSwiper", {
+      slidesPerView: 5,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+    //سکشن7
+    var swiper = new Swiper(".ajharSwiper", {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+     var swiper = new Swiper(".web2Swiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      effect: "fade",
+      autoplay: {
+        delay: 3400,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      loop: true,
+    });
+    //صفحه کلا
+        var swiper = new Swiper(".kala-digitalSwiper", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+          loop: true,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+          loop: true,
+        },
+        1024: {
+          slidesPerView: 7,
+          spaceBetween: 50,
+          loop: true,
+        },
+      },
+    });
+
